993f9890afc3a94d704889f4bd3f6cc182a44f0a
y-okamoto-1113 <okamoto.yuuki.1113+github@gmail.com>
2024-07-13 00:44:18 +0900

    wip

---
+++ asdf_install.sh
@@ -0,0 +1,32 @@
+plugins=(
+  nodejs
+  ruby
+  golang
+  python
+  terraform
+)
+
+for p in ${plugins[@]}; do
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "====================================================================================="
+  asdf plugin add $p
+  echo "====================================================================================="
+  asdf install $p latest
+  echo "====================================================================================="
+  asdf global $p latest
+done
+
+# asdfの使い方
+# @see https://asdf-vm.com/ja-jp/manage/commands.html
+
+# インストール済みのプラグイン一覧
+# asdf plugin list
+
+# ローカルでバージョン設定
+# asdf local ruby latest
+
+# インストールできるバージョン一覧確認
+# asdf list all terraform
+
+# インストール済みのバージョン一覧確認
+# asdf list
