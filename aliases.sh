90e245ef13627bf449ef2a1b9e9e8a1864ddb2f9
y-okamoto-1113 <okamoto.yuuki.1113+github@gmail.com>
2024-07-15 19:31:57 +0900

    wip

--- aliases.sh
+++ aliases.sh
@@ -6,9 +6,10 @@
 alias l='eza --header --git --git-repos --time-style=long-iso --long --all --group'
 alias lt='eza --icons --tree --all -L 2'
 alias cat='ccat'
-alias reload='source ~/.zshenv && source ~/.zshrc'
+alias reload='source ~/.zshrc'
 
 # ==============================
 # Docker
 # ==============================
 alias dc='docker compose'
+alias dc-reload='dc down && dc up'
